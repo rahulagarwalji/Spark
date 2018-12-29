7745ab6d0814c6d591041ad8381691f0bd80f020
Rahul Agarwal <rahulagarwalji@gmail.com>
2018-12-29 12:27:52 -0800

    add new file to branch D-1001

--- download_from_s3.py
+++ download_from_s3.py
@@ -1,18 +1,6 @@
- # import required python packages
-import boto3
-import botocore
+bucketName = "Your S3 BucketName"
+Key = "Original Name and type of the file you want to upload into s3"
+outPutname = "Output file name(The name you want to give to the file after we upload to s3)"
 
-Bucket = "Your S3 Global Bucket Name"
-Key = "Name of the file in S3 that you want to download"
-outPutName = "Output file name(The name you want to save after we download from s3)"
-
-# create boto3 resource object
-s3 = boto3.resource('s3')
-try:
-    s3.Bucket(Bucket).download_file(Key, outPutName)
-except botocore.exceptions.ClientError as e:
-    if e.response['Error']['Code'] == "404":
-        print("The object does not exist")
-    else:
-		print("Error while downloading the file to S3 " + str(e))
-        raise
+s3 = boto3.client('s3')
+s3.upload_file(Key,bucketName,outPutname)
